b9171db7596dc77eb11410b428cb4c92e9218f1e
LeonardoGonzalez1717 <leitogonza1717@gmail.com>
2025-09-28 14:44:02 -0400

    segundo commit

--- template/header.php
+++ template/header.php
@@ -1,10 +1,19 @@
+<?php require 'connection.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INVERCLINIK</title>
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="index.css">
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+
+    <link href="assets/css/custom.css" rel="stylesheet" />
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://kit.fontawesome.com/a78fdc4d64.js" crossorigin="anonymous"></script>
+    
 </head>
